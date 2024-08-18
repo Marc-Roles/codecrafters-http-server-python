a28bc0c28942fe31cefa634ac142688bcee07b7c
Marc Roles <marcroles507@gmail.com>
2024-08-18 13:32:39 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,22 +1,27 @@
 import socket
 
-
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    client_socket, client_address = server_socket.accept()
     
-    request = client_socket.recv(1024).decode("utf-8")
+    while True:
+        
+        client_socket, client_address = server_socket.accept()
+        data = client_socket.recv(1024)
+        request_data = data.decode().split("\r\n")
+        request_line = request_data[0]
 
-    url = request.split()[1]
+        method, path, http_version = request_line.split()
 
-    if url != "/":
-        response = "HTTP/1.1 404 Not Found\r\n\r\n"
-    else:
-        response = "HTTP/1.1 200 OK\r\n\r\n"
+        if path == "/" and method == "GET":
+            response = "HTTP/1.1 200 OK\r\n\r\n"
+        elif path.startswith("/echo/") and method == "GET":
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
+        else:
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
-    client_socket.sendall(response.encode("utf-8"))
-    client_socket.close()
+        client_socket.sendall(response.encode("utf-8"))
+        client_socket.close()
 
 if __name__ == "__main__":
     main()
