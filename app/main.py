4432fa86eaf28a2f796248bfdf5317830c39f1d5
Marc Roles <marcroles507@gmail.com>
2024-08-18 14:01:48 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,11 +5,12 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     
     while True:
-        
+
         client_socket, client_address = server_socket.accept()
         data = client_socket.recv(1024)
         request_data = data.decode().split("\r\n")
         request_line = request_data[0]
+        user_agent = next((request.split(": ")[1] for request in request_data if request.startswith("User-Agent:")), "")
 
         method, path, http_version = request_line.split()
 
@@ -17,6 +18,8 @@ def main():
             response = "HTTP/1.1 200 OK\r\n\r\n"
         elif path.startswith("/echo/") and method == "GET":
             response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
+        elif path == "/user-agent":
+            response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}lol"
         else:
             response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
