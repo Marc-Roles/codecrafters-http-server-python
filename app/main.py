7631d26aa9ce3cdadbf53aef19ace962bba241d1
Marc Roles <marcroles507@gmail.com>
2024-08-18 13:08:17 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,8 +4,19 @@ import socket
 def main():
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    client_socket, client_address = server_socket.accept()
+    
+    request = client_socket.recv(1024).decode("utf-8")
 
+    url = request.split()[1]
+
+    if url != "/":
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    else:
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+
+    client_socket.sendall(response.encode("utf-8"))
+    client_socket.close()
 
 if __name__ == "__main__":
     main()
